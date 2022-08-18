6f17018c49247e41bd21833e85305fe621e45bc5
seonghye.kim <seonghye.kim@cndfactory.com>
2022-08-18 20:38:42 +0900

    20220818 dir refactoring and 20220818_star study update

---
+++ practice/20220818_star.js
@@ -0,0 +1,23 @@
+// console.log("Hello world");
+
+const num = 10;
+let star1 = '';
+for(let i=0; i<num; i++){
+    star1 += "★"
+    console.log(star1);
+}
+//
+//
+// let star2 = "★\n".repeat(10);
+// console.log(star2);
+
+
+let star3 = '';
+for(let i=10; i>0; i--){
+    // for(let i=0; i<num; i++){
+    //     star3 += '1';
+    // }
+    star3 += '1';
+
+    console.log(star3);
+}
